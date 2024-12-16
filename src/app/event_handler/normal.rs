1151c594fbacfcb68c3dade1bb28ad79c51a242a
Bruno Belarte <bruno.belarte@gmail.com>
2024-12-16 19:05:47 +0100

    refactor event handler for readability

--- src/app/event_handler/normal.rs
+++ src/app/event_handler/normal.rs
@@ -4,50 +4,23 @@ use crate::app::{app::App, app_state::State, card_editor::CardEditor, save_to_fi
 
 pub fn handler<'a>(app: &mut App, key_event: KeyEvent) -> State<'a> {
     match key_event.code {
-        KeyCode::Char('h') | KeyCode::Left => {
-            app.select_prev_column();
-            State::Normal
-        }
-        KeyCode::Char('j') | KeyCode::Down => {
-            app.select_next_card();
-            State::Normal
-        }
-        KeyCode::Char('k') | KeyCode::Up => {
-            app.select_prev_card();
-            State::Normal
-        }
-        KeyCode::Char('l') | KeyCode::Right => {
-            app.select_next_column();
-            State::Normal
-        }
-        KeyCode::Char('H') => {
-            app.mark_card_undone();
-            State::Normal
-        }
-        KeyCode::Char('J') => {
-            app.decrease_priority();
-            State::Normal
-        }
-        KeyCode::Char('K') => {
-            app.increase_priority();
-            State::Normal
-        }
-        KeyCode::Char('L') => {
-            app.mark_card_done();
-            State::Normal
-        }
-        KeyCode::Char('i') => match app.insert_card() {
-            Some(card) => State::Edit {
-                editor: CardEditor::new(card),
-            },
-            None => State::Normal,
-        },
-        KeyCode::Char('e') | KeyCode::Enter => match app.get_selected_card() {
-            Some(card) => State::Edit {
-                editor: CardEditor::new(card),
-            },
-            None => State::Normal,
-        },
+        // Card navigation
+        KeyCode::Char('h') | KeyCode::Left => navigate(app, Navigation::PrevColumn),
+        KeyCode::Char('j') | KeyCode::Down => navigate(app, Navigation::NextCard),
+        KeyCode::Char('k') | KeyCode::Up => navigate(app, Navigation::PrevCard),
+        KeyCode::Char('l') | KeyCode::Right => navigate(app, Navigation::NextColumn),
+
+        // Card marking
+        KeyCode::Char('H') => card_marking(app, Operation::MarkUndone),
+        KeyCode::Char('J') => card_marking(app, Operation::DecreasePriority),
+        KeyCode::Char('K') => card_marking(app, Operation::IncreasePriority),
+        KeyCode::Char('L') => card_marking(app, Operation::MarkDone),
+
+        // Card edition
+        KeyCode::Char('i') => card_edition(app, Edition::InsertAtCurrentPosition),
+        KeyCode::Char('e') | KeyCode::Enter => card_edition(app, Edition::EditCurrent),
+
+        // Other operations
         KeyCode::Esc => {
             app.disable_selection();
             State::Normal
@@ -63,6 +36,61 @@ pub fn handler<'a>(app: &mut App, key_event: KeyEvent) -> State<'a> {
         }
         KeyCode::Char('?') => State::Help,
         _ => State::Normal,
+    }
+}
+
+enum Navigation {
+    PrevColumn,
+    NextColumn,
+    PrevCard,
+    NextCard,
+}
+
+fn navigate<'a>(app: &mut App, nav: Navigation) -> State<'a> {
+    match nav {
+        Navigation::PrevColumn => app.select_prev_column(),
+        Navigation::NextColumn => app.select_next_column(),
+        Navigation::PrevCard => app.select_prev_card(),
+        Navigation::NextCard => app.select_next_card(),
+    }
+
+    State::Normal
+}
+
+enum Operation {
+    MarkUndone,
+    DecreasePriority,
+    IncreasePriority,
+    MarkDone,
+}
+
+fn card_marking<'a>(app: &mut App, operation: Operation) -> State<'a> {
+    match operation {
+        Operation::MarkUndone => app.mark_card_undone(),
+        Operation::DecreasePriority => app.decrease_priority(),
+        Operation::IncreasePriority => app.increase_priority(),
+        Operation::MarkDone => app.mark_card_done(),
+    }
+
+    State::Normal
+}
+
+enum Edition {
+    EditCurrent,
+    InsertAtCurrentPosition,
+}
+
+fn card_edition<'a>(app: &mut App, operation: Edition) -> State<'a> {
+    let card = match operation {
+        Edition::EditCurrent => app.get_selected_card(),
+        Edition::InsertAtCurrentPosition => app.insert_card(),
+    };
+
+    match card {
+        Some(card) => State::Edit {
+            editor: CardEditor::new(card),
+        },
+        None => State::Normal,
     }
 }
 
